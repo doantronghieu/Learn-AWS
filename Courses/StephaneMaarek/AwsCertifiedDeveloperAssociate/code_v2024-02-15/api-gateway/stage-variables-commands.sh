c15c205dde3cca8a3a1b3755baaeb788fa5c36b5
Doan Trong Hieu <harrisondoan2810@gmail.com>
2024-05-14 10:15:12 +0700

    Refactor Code

--- Courses/StephaneMaarek/AwsCertifiedDeveloperAssociate/code_v2024-02-15/api-gateway/stage-variables-commands.sh
+++ Courses/StephaneMaarek/AwsCertifiedDeveloperAssociate/code_v2024-02-15/api-gateway/stage-variables-commands.sh
@@ -7,3 +7,11 @@ aws lambda add-permission   --function-name "arn:aws:lambda:eu-west-2:3871241233
  
 # For the PROD alias (replace the region and account number accordingly)
 aws lambda add-permission   --function-name "arn:aws:lambda:eu-west-2:387124123361:function:lambda-api-gateway-proxy-root-get:PROD"   --source-arn "arn:aws:execute-api:eu-west-2:387124123361:kfz6b0ty9i/*/GET/stagevariables"   --principal apigateway.amazonaws.com   --statement-id eb4791cf-60c1-4a0f-b519-bbdb1ff13114   --action lambda:InvokeFunction --region eu-west-2
+
+
+aws lambda add-permission \
+--function-name "arn:aws:lambda:us-east-1:360110288814:function:api-gateway-stage-variables-get:PROD" \
+--source-arn "arn:aws:execute-api:us-east-1:360110288814:2gsehf5fme/*/GET/stage-variables" \
+--principal apigateway.amazonaws.com \
+--statement-id 753100dd-36a1-4e53-92ba-2f1442861a7c \
+--action lambda:InvokeFunction
